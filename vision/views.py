8d32300a51bd2bbccc5e2a2207e5a70377464061
MaximPod <maxim@universe.su>
2019-03-08 01:32:08 +0700

    v2

--- vision/views.py
+++ vision/views.py
@@ -3,7 +3,11 @@
 from django.shortcuts import render, get_object_or_404
 #from .forms import PostForm
 #from django.shortcuts import redirect
+import os
 
 # Create your views here.
 def base(request):
-    return  render(request, 'base.html', {})
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "vision/static"), ]
+    data = {'BASE_DIR':BASE_DIR, 'STATICFILES_DIRS':STATICFILES_DIRS}
+    return  render(request, 'base.html', context=data)
